10aa38dfb317782beb43e33c911627804fb24019
Mariano2nd <godinezjoseph16@gmail.com>
2024-10-17 19:21:43 -0700

    changed multiagent minimax function

--- multiAgents.py
+++ multiAgents.py
@@ -16,14 +16,33 @@ def minimax(game_state: GameStatus, depth: int, maximizingPlayer: bool, alpha=fl
 		newScores = game_state.get_scores(terminal)
 		return newScores, None
 	
+	#
+	availableMoves = []
+	for i in range(len(game_state.board_state)):
+		for j in range(len(game_state.board_state[i])):
+			# print('This at i and j→', game_state.board_state[i][j])
+			if game_state.board_state[i][j] == 0:
+				availableMoves.append((i,j))
+				
+	# print(availableMoves)
+	# #input()
+	
 	# This section of code is for our maximizing agent
 	if maximizingPlayer:
 		# The max evaluation is set to an arbitrary small value by default
 		# so that the first value compared with it will take it's place 
-		maxEval = float("-inf")
+		# maxEval = alpha
 		# We obtain a list of available moves by calling the get_moves()
 		# function in the GameStatus file
-		availableMoves = game_state.get_moves()
+		# availableMoves = []
+		# for i in range(len(game_state.board_state)):
+		# 	print('This is the 1 row ', i)
+		# 	for j in range(len(game_state.board_state[i])):
+		# 		print('\tThis is the 1 col ', j)
+		# 		if not j in game_state.get_moves():
+		# 			availableMoves.append((i,j))
+		# print(availableMoves)
+		# #input()
 		
 		# Conduct the recursive calls that switch between the Max and Min
 		# agents for each possible move on the board. The best move is checked
@@ -31,12 +50,25 @@ def minimax(game_state: GameStatus, depth: int, maximizingPlayer: bool, alpha=fl
 		for position in availableMoves:
 			# Pass in (depth - 1) to move down a level and "False" to give control
 			# to the minimizing agent
-			currEval = minimax(game_state, depth - 1, False, alpha, beta)[0]
+			print("game state before func get_new_state() →", game_state.board_state)
+			new_game_state=game_state.get_new_state(position)
+			print("\tgame state before minimax →", game_state.board_state)
+			print("new_game_state before minimax →", game_state.board_state)
+			currEval,currMove = minimax(new_game_state, depth - 1, False, alpha, beta)
+			print("new_game_state after minimax →", game_state.board_state)
+			print("\tgame state after minimax →", game_state.board_state)
+			bestMove=position
+			print("best move before alpha",bestMove)
+			#input()
 			# If the current eval has a better score than our highest known score,
 			# set the bestMove to the current position in the loop
-			if currEval > maxEval:
-				bestMove = position
-				maxEval = currEval
+			if currEval > alpha:
+				# print(maxEval)
+				if currMove:
+					bestMove = currMove
+				# maxEval = currEval
+				# alpha = currEval
+				print('best moves',bestMove,'and new alpha is', alpha)
 				
 			# Need to see if this approach is allowed / works	
 			"""	
@@ -52,35 +84,48 @@ def minimax(game_state: GameStatus, depth: int, maximizingPlayer: bool, alpha=fl
 					
 			# alpha is set to the larger value between itself and the currEval.
 			# Then, if the beta is <= alpha, the branch is pruned
+			print("1Check alpha > currEval?",alpha, currEval)
 			alpha = max(alpha, currEval)
+			print("2Check alpha > currEval?",alpha, currEval)
 			if beta <= alpha:
 				break
 			
 		# Returns the best possible score for the maximizing agent and the corresponding move
-		return maxEval, bestMove
+		return currEval,bestMove
 			
 			
 	# This section of code is for our minimizing agent
 	else:
 		# The min evaluation is set to an arbitrary large value by default
 		# so that the first value compared with it will take it's place 
-		minEval = float("inf")
+		# minEval = beta
 		# We obtain a list of available moves by calling the get_moves()
 		# function in the GameStatus file
-		availableMoves = game_state.get_moves()
-		
 		# Conduct the recursive calls that switch between the Max and Min
 		# agents for each possible move on the board. The best move is checked
 		# at each step by evaluating the corresponding game score.
 		for position in availableMoves:
 			# Pass in (depth - 1) to move down a level and "True" to give control
 			# to the maximizing agent
-			currEval = minimax(game_state, depth - 1, True, alpha, beta)[0]
+			print("game state before func get_new_state() →", game_state.board_state)
+			new_game_state=game_state.get_new_state(position)
+			print("\tgame state before minimax →", game_state.board_state)
+			print("new_game_state before minimax →", game_state.board_state)
+			currEval,currMove = minimax(new_game_state, depth - 1, True, alpha, beta)
+			print("new_game_state after minimax →", game_state.board_state)
+			print("\tgame state after minimax →", game_state.board_state)
+			bestMove=position
+			print("best move before beta",bestMove)
 			# If the current eval has a lower score than our lowest known score,
 			# set the bestMove to the current position in the loop
-			if currEval < minEval:
-				bestMove = position
-				minEval = currEval
+			if currEval < beta:
+				
+				if currMove:
+					bestMove = currMove
+				# minEval = currEval
+				# beta = currEval
+				print('best moves',bestMove,'and new beta is', beta)
+				#input()
 				
 			# Need to see if this approach is allowed / works	
 			"""	
@@ -96,12 +141,15 @@ def minimax(game_state: GameStatus, depth: int, maximizingPlayer: bool, alpha=fl
 					
 			# beta is set to the smaller value between itself and the currEval.
 			# Then, if the beta is <= alpha, the branch is pruned
+			print("1Check beta < currEval?",beta, currEval)
 			beta = min(beta, currEval)
+			print("2Check beta < currEval?",beta, currEval)
 			if beta <= alpha:
 				break
 					
 		# Returns the best possible score for the minimizing agent and the corresponding move
-		return minEval, bestMove
+		print('final eval', currEval,'and best move',bestMove)
+		return currEval, currMove
 				
 
 	"""
